a421cd00fe4f7fe7e749fefe646559983ccbc8ee
Oskii <94227871+Oskii0201@users.noreply.github.com>
2023-01-11 20:24:43 +0100

    Połączenia z bazą dla wszystkich

--- DbConnect.cs
+++ DbConnect.cs
@@ -9,8 +9,12 @@ namespace Cinema_ticket_system
     //klasa połączenia z bazą danych
     public class DbConnect
     {
-                                                                                                           /* ścieżka do pliku z bazą danych Cienema.mdf */
+        //Oskar                                                                                                   /* ścieżka do pliku z bazą danych Cienema.mdf */
         public SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\oskar\source\repos\Oskii0201\Cinema-ticket-system\Cinema.mdf;Integrated Security=True;Connect Timeout=30");
+        //Mateusz
+        //public SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\oskar\source\repos\Oskii0201\Cinema-ticket-system\Cinema.mdf;Integrated Security=True;Connect Timeout=30");
+        //Justyna
+        //public SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\oskar\source\repos\Oskii0201\Cinema-ticket-system\Cinema.mdf;Integrated Security=True;Connect Timeout=30");
         public SqlCommand cmd = new SqlCommand();
         public SqlDataReader dr;
     }
